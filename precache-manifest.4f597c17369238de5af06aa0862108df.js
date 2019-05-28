437eeecac1c8996b0022db28159220decb14b71c
Slendos <slendos123@gmail.com>
2019-05-28 21:29:22 +0200

    Updates

--- precache-manifest.4f597c17369238de5af06aa0862108df.js
+++ precache-manifest.4f597c17369238de5af06aa0862108df.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/react-chessGame/static/media/restart-button.1d250b39.jpg"
   },
   {
-    "revision": "2352380cca80eb0884064c8dd0e16286",
-    "url": "/react-chessGame/static/media/chess-project.2352380c.png"
+    "revision": "95d44f2539968f885a48f4b0417404af",
+    "url": "/react-chessGame/static/media/chess-project.95d44f25.png"
   },
   {
     "revision": "30e01051322bb9baa4ff6ed8a5bdc770",
@@ -16,23 +16,23 @@ self.__precacheManifest = [
     "url": "/react-chessGame/static/js/runtime~main.453e6949.js"
   },
   {
-    "revision": "9af70506fab35bb545b1",
-    "url": "/react-chessGame/static/js/main.0e5079d6.chunk.js"
+    "revision": "51e58d0456f21738f9c3",
+    "url": "/react-chessGame/static/js/main.a0c7f1db.chunk.js"
   },
   {
     "revision": "0a9fb2d3ad269f436ecb",
     "url": "/react-chessGame/static/js/2.b6336dce.chunk.js"
   },
   {
-    "revision": "9af70506fab35bb545b1",
-    "url": "/react-chessGame/static/css/main.14ebd225.chunk.css"
+    "revision": "51e58d0456f21738f9c3",
+    "url": "/react-chessGame/static/css/main.e9183e27.chunk.css"
   },
   {
     "revision": "0a9fb2d3ad269f436ecb",
     "url": "/react-chessGame/static/css/2.189c9f69.chunk.css"
   },
   {
-    "revision": "8784558c7ae775843eacca89d53ebf93",
+    "revision": "da33bf0f429858efe429e63de5b98dfd",
     "url": "/react-chessGame/index.html"
   }
 ];
