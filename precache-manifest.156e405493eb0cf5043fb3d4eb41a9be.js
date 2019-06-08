0f8ba2af37ce8af803665cc36a2df66bc9763d4a
Slendos <slendos123@gmail.com>
2019-06-08 12:42:41 +0200

    Updates

--- precache-manifest.156e405493eb0cf5043fb3d4eb41a9be.js
+++ precache-manifest.156e405493eb0cf5043fb3d4eb41a9be.js
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/react-chessGame/static/js/runtime~main.453e6949.js"
   },
   {
-    "revision": "30bbb09a7aab528d413c",
+    "revision": "2b7cd3fc1ca912ef8f4f",
     "url": "/react-chessGame/static/js/main.ba65771b.chunk.js"
   },
   {
@@ -24,15 +24,15 @@ self.__precacheManifest = [
     "url": "/react-chessGame/static/js/2.1720cbc1.chunk.js"
   },
   {
-    "revision": "30bbb09a7aab528d413c",
-    "url": "/react-chessGame/static/css/main.8b5ed922.chunk.css"
+    "revision": "2b7cd3fc1ca912ef8f4f",
+    "url": "/react-chessGame/static/css/main.d931081c.chunk.css"
   },
   {
     "revision": "7184534e5988228742e3",
     "url": "/react-chessGame/static/css/2.f992f7d0.chunk.css"
   },
   {
-    "revision": "a0e4fcaca2379003814bfbf6d6ea6f6a",
+    "revision": "34d5cd3424746309068ee77926a12d0a",
     "url": "/react-chessGame/index.html"
   }
 ];
